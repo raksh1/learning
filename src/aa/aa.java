77b75127b4d9bad6caa126298e082883b374c5e1
hostbooks <@>
2022-07-28 21:11:38 +0530

    Initial commit

---
+++ src/aa/aa.java
@@ -0,0 +1,9 @@
+package aa;
+
+import java.time.Clock;
+
+public class aa {
+    public  static  void main(String args[]){
+        System.out.println("hi");
+    }
+}
---
+++ src/bb/bb.java
@@ -0,0 +1,8 @@
+package bb;
+
+public class bb {
+    public  static  void main(String args[]){
+        System.out.println("hi");
+        System.out.println();
+    }
+}
